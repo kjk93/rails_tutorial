45928d0bc67f593f070e5040d1eb8f4e7ea799e7
Kyle Kravette <kkravette@gmail.com>
2015-06-30 18:16:30 -0400

    Fixed sendgrid error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,13 +66,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :stmp
   host = 'kjk-rails-tutorial.herokuapp.com'
   config.action_mailer.default_url_options = {host: host}
-  ActionMailer::Base.smtp_settings{
-    :address => 'stmp.sendgrid.net',
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com'
+    :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
 
