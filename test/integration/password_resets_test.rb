c06eee2f01e6340bc7c9fa1e8befd3227fdc823d
Kyle Kravette <kkravette@gmail.com>
2015-06-30 22:27:50 -0400

    finished exercises

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -4,7 +4,6 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
 	def setup
 		ActionMailer::Base.deliveries.clear
 		@user = users(:michael)
-
 	end
 
 	test "password resets" do
@@ -49,5 +48,17 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
 		assert is_logged_in?
 		assert_not flash.empty?
 		assert_redirected_to user
+	end
+
+	test "expired token" do
+		get new_password_reset_path
+		post password_resets_path, password_reset: {email: @user.email}
+
+		@user = assigns(:user)
+		@user.update_attribute(:reset_sent_at, 3.hours.ago)
+		patch password_reset_path(@user.reset_token), email: @user.email, user: {password: "foobar", password_confirmation: "foobar"}
+		assert_response :redirect
+		follow_redirect!
+		assert_match 'expired', response.body
 	end
 end
