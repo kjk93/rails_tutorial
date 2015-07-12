0c52574418b086b8cb5e0afb977395a487e87437
Kyle Kravette <kkravette@gmail.com>
2015-07-12 18:03:01 -0400

    working on aws issue

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
 			#configuration for amazon s3
 			:provider => 'AWS',
 			:aws_access_key_id => ENV['AKIAJXXD3FH2MWM4MF6A'],
-			:aws_secret_access_key_id => ENV['WldDOlpoE/uKAz9qbQmf7ij4WUpKX3/XinjyLV4l']
+			:aws_secret_access_key => ENV['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']
 		}
 		config.fog_directory = ENV['rails-tutorial-kjk']			
 	end
