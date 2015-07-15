8fb9a1234b87beb4ee8a1a61c0995e5fa2b9c3fa
Kyle Kravette <kkravette@gmail.com>
2015-07-14 21:13:40 -0400

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -68,9 +68,10 @@ class User < ActiveRecord::Base
 		reset_sent_at < 2.hours.ago
 	end
 
-	#defines a proto-feed
+	#returns a users status feed
 	def feed
-		Micropost.where("user_id=?", id)
+		following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+		Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
 	end
 
 	#follows a user
